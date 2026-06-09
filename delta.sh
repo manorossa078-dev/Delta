a872363e7424718d20f39a8ab23e374cb285da60
manorossa078-dev <manorossa078@gmail.com>
2026-06-09 16:38:05 +0200

    Add files via upload

---
+++ delta.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+GREEN="\e[1;32m"
+RED="\e[1;31m"
+DEFAULT="\e[0m"
+FILETYPES=("zip" "7z")
+VERSION="v.0.0.4"
+
+echo -e "${RED}"
+
+cat << "EOF"
+ ____       _ _           ___  ____ ___ _   _ _____
+|  _ \  ___| | |_ __ _   / _ \/ ___|_ _| \ | |_   _|
+| | | |/ _ \ | __/ _` | | | | \___ \| ||  \| | | |
+| |_| |  __/ | || (_| | | |_| |___) | || |\  | | |
+|____/ \___|_|\__\__,_|  \___/|____/___|_| \_| |_|
+EOF
+
+echo -e "Delta OSINT $VERSION${GREEN}"
+
+if [ -z "$1" ]; then
+    echo -e "${RED}Error. Enter a flag (use -h to see the options).${DEFAULT}" >&2
+    exit 1
+elif [ "$1" == "-h" ]; then
+    echo "-------------------------------------------------------------"
+    echo "Use -h to view this message"
+    echo "Use -s [username] to search for a username"
+    echo "Use -c followed by your wordlist path to crack an archive password"
+    echo "Use -cw followed by your wordlist path to crack a website"
+    echo "-------------------------------------------------------------"
+elif [ "$1" == "-s" ]; then
+    # Fixed: Wrapped $2 in quotes to prevent a syntax crash if empty
+    if [ -z "$2" ]; then
+        echo -e "${RED}Error. Enter a username.${DEFAULT}" >&2
+    else
+        echo -e "Searching for $2...${DEFAULT}"
+        sherlock --print-found "$2"
+        echo -e "${GREEN}Done searching.${DEFAULT}"
+    fi
+elif [ "$1" == "-c" ]; then
+	read -p "Enter the target file: " target
+	    read -p "Enter the file type: " filetype
+    echo "Available targets: zip, 7z"
+    
+    if [[ ! " ${FILETYPES[*]} " == *" $filetype "* ]]; then
+        echo -e "${RED}Error. File type not found.${DEFAULT}" >&2
+        exit 1
+    else
+        if [ "$filetype" == "zip" ]; then
+		if [ -z "$2" ]; then
+			echo -e "${RED}Error. Try providing a wordlist."
+			exit 1
+		else
+	            echo "Extracting password..."
+        	    zip2john "$target" > password.txt 2> /dev/null
+	            echo -e "Cracking password...${DEFAULT}"
+        	    john --wordlist="$2" password.txt
+		fi
+        else
+       	    echo "Extracting password..."
+            7z2john "$target" > password.txt 2> /dev/null
+       	    echo -e "Cracking password...${DEFAULT}"
+            john --wordlist="$2" password.txt
+        fi
+        john --show password.txt
+    fi
+elif [ "$1" == "-cw" ]; then
+	if [ -z "$2" ]; then
+		echo -e "${RED}Error. Put a wordlist path." >&2
+		exit 1
+	else
+		read -p "Enter an URL: " target
+		echo -e "Cracking websites's directories' passwords...${DEFAULT}"
+		gobuster dir -u $target -w $2
+		echo -e "${GREEN}Done cracking!"
+	fi
+else
+    echo -e "${RED}Error. Flag not recognized.${DEFAULT}" >&2
+fi
